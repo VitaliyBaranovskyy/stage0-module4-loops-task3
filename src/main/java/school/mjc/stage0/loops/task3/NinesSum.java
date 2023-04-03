70341e9d1bedf2e67b28fde4a2d15b594f1980e6
VitaliyBaranovskyy <40470956+VitaliyBaranovskyy@users.noreply.github.com>
2023-04-03 10:18:03 +0300

    Update NinesSum.java

--- src/main/java/school/mjc/stage0/loops/task3/NinesSum.java
+++ src/main/java/school/mjc/stage0/loops/task3/NinesSum.java
@@ -2,6 +2,18 @@ package school.mjc.stage0.loops.task3;
 
 public class NinesSum {
     public void calculateSum(int lengthOfLastNumber) {
-
+int sum=0;
+        int a = 9;
+        int i;
+        for (i=1; i<=lengthOfLastNumber; i++){
+            sum = sum + a;
+            System.out.println(a);
+            a= a*10+9;
+    }
+        System.out.println(sum);
+}
+public static void main(String[] args) {
+        NinesSum main = new NinesSum();
+        main.calculateSum(3);
     }
 }
