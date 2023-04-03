102f811198f5180f9f7041dfd6c54c5582569979
VitaliyBaranovskyy <40470956+VitaliyBaranovskyy@users.noreply.github.com>
2023-04-03 10:08:13 +0300

    Update AlphabetPrinter.java

--- src/main/java/school/mjc/stage0/loops/task3/AlphabetPrinter.java
+++ src/main/java/school/mjc/stage0/loops/task3/AlphabetPrinter.java
@@ -2,5 +2,15 @@ package school.mjc.stage0.loops.task3;
 
 public class AlphabetPrinter {
     public void printAlphabet() {
+    char abc ;
+        for (abc = 'A'; abc <= 'Z'; abc++) {
+
+            System.out.println(abc);    
+        
+    }
+}
+ public static void main(String[] args) {
+        AlphabetPrinter main = new AlphabetPrinter();
+        main.printAlphabet();
     }
 }
