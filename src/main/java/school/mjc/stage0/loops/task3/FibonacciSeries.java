b319e36f9883e5236a0eaba412977166930cb93c
VitaliyBaranovskyy <40470956+VitaliyBaranovskyy@users.noreply.github.com>
2023-04-03 10:20:07 +0300

    Update FibonacciSeries.java

--- src/main/java/school/mjc/stage0/loops/task3/FibonacciSeries.java
+++ src/main/java/school/mjc/stage0/loops/task3/FibonacciSeries.java
@@ -2,5 +2,21 @@ package school.mjc.stage0.loops.task3;
 
 public class FibonacciSeries {
     public void printFibonacci(int lastFibonacci) {
+        
+        int i = 0;
+      int b = 1;
+      int c;
+        System.out.print(i + " " + b + " ");
+
+        for (int a = 2; a <= lastFibonacci; a++){
+            c = i + b;
+            System.out.print(c + " ");
+            i=b;
+            b=c;
+    }
+}
+public static void main(String[] args) {
+        FibonacciSeries main = new FibonacciSeries();
+        main.printFibonacci(11);
     }
 }
